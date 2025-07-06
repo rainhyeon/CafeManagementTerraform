2dfd8f7a077a6b2695b554f0411b9d6b186c9451
rainhyeon <90149887+rainhyeon@users.noreply.github.com>
2025-07-07 00:25:44 +0900

    Update main.tf via Lambda at 2025-07-06T15:25:44.220093

--- main.tf
+++ main.tf
@@ -710,7 +710,7 @@ resource "aws_route53_record" "Cafe_Management_www_onprem" {
   records = ["34.22.91.176"]
 
   weighted_routing_policy {
-    weight = 50
+    weight = 0
   }
 
   set_identifier = "www-onprem-weight-225"
@@ -728,7 +728,7 @@ resource "aws_route53_record" "Cafe_Management_www_alb" {
   }
 
   weighted_routing_policy {
-    weight = 50
+    weight = 100
   }
 
   set_identifier = "www-alb-weight-0"
